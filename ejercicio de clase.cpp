00ccff6a73e39ce01b5b53591ad25253e1442a8a
Johan <106392284+Johan0202@users.noreply.github.com>
2022-06-18 22:21:21 -0600

    Add files via upload

---
+++ ejercicio de clase.cpp
@@ -0,0 +1,11 @@
+//Elabore un programa que muestre los multiplos de 3//
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=1; i<=150; i++){
+		cout<< i<<endl;
+		if(i%3 ==0)
+		cout<<i<<endl;
+	}
+	return 0;
+}
